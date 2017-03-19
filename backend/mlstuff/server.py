14f0a9e67885a6f5bd11f1d83a85cfeff3281bd4
teoh <mteohh@gmail.com>
2017-03-18 20:04:36 -0700

    fixed server.py to receive image properly and fixed notebook loss fxn

--- backend/mlstuff/server.py
+++ backend/mlstuff/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+import os
 
 app = Flask(__name__)
 
@@ -10,8 +11,12 @@ def hello():
 @app.route("/new_image", methods=['POST'])
 def new_image():
     if request.method == 'POST':
-        img = request.files['image']
-        return 'get it!'
+        if 'file' not in request.files:
+            return 'please make sure that the key is called "file"'
+        file = request.files['file']
+        file.save('./TEST.png')
+        return 'success!'
+       
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
