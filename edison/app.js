c39374602ece4ac99065c091585120846ff56ba4
Denis Trailin <denis.trailin@gmail.com>
2017-03-18 22:49:19 -0700

    syntax fix

--- edison/app.js
+++ edison/app.js
@@ -8,41 +8,42 @@ imgur.setClientID("f1adc9604875526");
 
 
 Cylon.robot({
-  connections: {
-    edison: { adaptor: 'intel-iot' }
-  },
-  devices: {
-    button: { driver: 'button', pin: 8, connection: 'edison'},
-    led: { driver: 'led', pin: 13 }
-  },
-  isEnabled: true,
-  reset: function() {
-    this.led.turnOff();
-  },
-  work: function(my) {
-    my.button.on('push', () => {
-        my.led.toggle();
-        setTimeout(() => {
-            childProcess.exec('/home/root/bin/ffmpeg/ffmpeg -f video4linux2  -s 1920x1080 -i /dev/video0 -vframes 1' + imageName, (error, stdout, stderr)=> {
+    connections: {
+        edison: {adaptor: 'intel-iot'}
+    },
+    devices: {
+        button: {driver: 'button', pin: 8, connection: 'edison'},
+        led: {driver: 'led', pin: 13}
+    },
+    isEnabled: true,
+    reset: function () {
+        this.led.turnOff();
+    },
+    work: function (my) {
+        my.button.on('push', () => {
+            my.led.toggle();
+            setTimeout(() => {
+                childProcess.exec('/home/root/bin/ffmpeg/ffmpeg -f video4linux2  -s 1920x1080 -i /dev/video0 -vframes 1' + imageName, (error, stdout, stderr) => {
 
-                console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                }
+                    console.log('stdout: ' + stdout);
+                    console.log('stderr: ' + stderr);
+                    if (error !== null) {
+                        console.log('exec error: ' + error);
+                    }
 
-                fs.readFile(imageName, function(err, original_data){
-                    var base64Image = original_data.toString('base64');
+                    fs.readFile(imageName, function (err, original_data) {
+                        var base64Image = original_data.toString('base64');
 
-                });
+                    });
 
-                imgur.upload(path.join(__dirname, imageName), function (err, res) {
-                    console.log(res.data.link); // Log the imgur url
-                });
+                    imgur.upload(path.join(__dirname, imageName), function (err, res) {
+                        console.log(res.data.link); // Log the imgur url
+                    });
 
-                my.led.toggle();
-      },500)
+                    my.led.toggle();
+                }, 500)
 
-    });
-  })
+            });
+        })
+    }
 }).start();
