ec2770cde5e3cf14c739ea4e3ba1db32674817ad
Denis Trailin <denis.trailin@gmail.com>
2017-03-18 22:39:55 -0700

    added image upload

--- edison/app.js
+++ edison/app.js
@@ -1,4 +1,12 @@
 var Cylon = require('cylon');
+var childProcess = require('child_process');
+var imgur = require('imgur-node-api'),
+    path = require('path');
+
+const imageName = "image.png";
+imgur.setClientID("f1adc9604875526");
+
+
 Cylon.robot({
   connections: {
     edison: { adaptor: 'intel-iot' }
@@ -12,9 +20,29 @@ Cylon.robot({
     this.led.turnOff();
   },
   work: function(my) {
-    my.button.on('push', function() {
-        console.log("pushed");
+    my.button.on('push', () => {
         my.led.toggle();
+        setTimeout(() => {
+            childProcess.exec('/home/root/bin/ffmpeg/ffmpeg -f video4linux2  -s 1920x1080 -i /dev/video0 -vframes 1' + imageName, (error, stdout, stderr)=> {
+
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                }
+
+                fs.readFile(imageName, function(err, original_data){
+                    var base64Image = original_data.toString('base64');
+
+                });
+
+                imgur.upload(path.join(__dirname, imageName), function (err, res) {
+                    console.log(res.data.link); // Log the imgur url
+                });
+
+                my.led.toggle();
+      },500)
+
     });
-  }
+  })
 }).start();
